4ac3dc7349a718e622e8ede6e531d70b6de71fa1
Vladimir Rogojin <vrogojin@abo.fi>
2017-02-27 15:43:33 +0200

    pretifyied the waiting message

--- start_net_control.sh
+++ start_net_control.sh
@@ -7,7 +7,7 @@ echo "<em>SESSION: $SESSION_ID<br/>" >> result.html
 echo "Please wait!</br>" >> result.html
 echo "Analyzing the network ...<br/>" >> result.html
 echo "<br/>" >> result.html
-echo "You will be notified by email when the results of the analysis are ready<br/></em>" >> result.html
+echo "The pipeline is doing its best to deliver the results to you ASAP. This computation may take loooong time<br/></em>" >> result.html
 cat ../../result_page_template_footer.html >> result.html
 
 echo $SESSION_ID > session_id
