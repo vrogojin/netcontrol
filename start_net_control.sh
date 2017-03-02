808aa5b573156af44cd78be445f88a5d8ffdc6d8
Vladimir Rogojin <vrogojin@abo.fi>
2017-03-02 16:12:22 +0200

    added loading sign

--- start_net_control.sh
+++ start_net_control.sh
@@ -5,6 +5,7 @@ echo	$SESSION_ID >> ../../sessions.txt
 cat ../../result_page_template_header.html > result.html
 echo "<em>SESSION: $SESSION_ID<br/>" >> result.html
 echo "Please wait!</br>" >> result.html
+echo "<img src=\"https://upload.wikimedia.org/wikipedia/commons/3/3a/Gray_circles_rotate.gif\" width=\"48px\" height=\"47px\"><br>" >> result.html
 echo "Analyzing the network ...<br/>" >> result.html
 echo "<br/>" >> result.html
 echo "The pipeline is doing its best to deliver the results to you ASAP. This computation may take loooong time<br/></em>" >> result.html
