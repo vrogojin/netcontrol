3cfc155337cc17dd5837c52f3e361e60062a32cd
Vladimir Rogojin <vrogojin@abo.fi>
2017-03-03 14:37:07 +0200

    skipping first 'debug' line of ns_result

--- makezip.sh
+++ makezip.sh
@@ -6,7 +6,7 @@ cp @var1@ result/visual.pdf
 cp @var2@ result/
 cp @var3@ result/driven.csv
 cp @var4@ result/extra.csv
-cp @var5@ result/
+cp @var5@ result/details.txt
 cp @var6@ result/message.txt
 cp @var6@ result/res
 
